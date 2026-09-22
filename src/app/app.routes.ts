554a7e1e7b95163c84fe02e33f5e85c43668e447
YhoshuaCZ <u20231h117@upc.edu.pe>
2026-09-22 11:28:54 -0500

    feat(about): Add About view with title and commit changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import {Home} from './shared/presentation/views/home/home';
+import {About} from './shared/presentation/views/about/about';
 
 const baseTitle = 'ACME Learning Center';
 
+const about= () =>
+  import('./shared/presentation/views/about/about').then(m => m.About);
+
 export const routes: Routes = [
   { path: 'home', component: Home, title: `${baseTitle} - Home` },
+  { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
 ];
